3a00f3a9f0469a2f9a77d622b7b4a49f01613ed3
puerchik <rumyantsevnn@yandex.ru>
2024-09-07 16:22:15 +0300

    dark theme set in storybook

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,7 +1,23 @@
 import type { Preview } from '@storybook/react'
+import { themes } from '@storybook/theming'
+import '../src/styles/index.scss'
 
 const preview: Preview = {
+  
   parameters: {
+    actions: { argTypesRegex: '^on[A-Z].*' },
+    docs: {
+      theme: themes.dark,
+    },
+    backgrounds: {
+      default: 'dark',
+      values: [
+        {
+          name: 'dark',
+          value: '#000000',
+        },
+      ],
+    },
     controls: {
       matchers: {
         color: /(background|color)$/i,
