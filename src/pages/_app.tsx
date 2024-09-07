3ff85cb2f0b0e29d82bf0e3bd7c374c8db2040cc
puerchik <rumyantsevnn@yandex.ru>
2024-09-07 10:33:51 +0300

    Added reset of standard styles, some standard values ​​and fonts.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import type { AppProps } from 'next/app'
 
 import './../styles/index.scss'
+import '@fontsource-variable/inter'
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
