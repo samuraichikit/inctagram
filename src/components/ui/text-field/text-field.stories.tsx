a6e2006e5e44301c7f8f4cb562d1ba94da9d2b04
puerchik <rumyantsevnn@yandex.ru>
2024-09-11 10:24:13 +0300

    INC-11 feat: add basic markup and styles for the input

--- src/components/ui/text-field/text-field.stories.tsx
+++ src/components/ui/text-field/text-field.stories.tsx
@@ -1,16 +1,23 @@
-import { Meta } from '@storybook/react'
+import { Meta, StoryObj } from '@storybook/react'
 
 import { TextField } from './text-field'
 
 const meta = {
   component: TextField,
+  tags: ['autodocs'],
+  title: 'Components/TextField',
 } satisfies Meta<typeof TextField>
 
 export default meta
+type Story = StoryObj<typeof meta>
 
-export const Primary = {
+export const Text: Story = {
   args: {
+    disabled: false,
+    error: '',
+    label: 'Email',
     placeholder: 'Hello',
+    type: 'email',
   },
 }
 
--- src/components/ui/text-field/text-field.tsx
+++ src/components/ui/text-field/text-field.tsx
@@ -1,15 +1,37 @@
-import { ComponentProps } from 'react'
+import { ComponentProps, forwardRef } from 'react'
 
+import { Typography } from '@/components/ui/typography'
 import clsx from 'clsx'
 
 import s from './text-field.module.scss'
 
-type Props = ComponentProps<'input'>
+type Props = {
+  error?: string
+  label?: string
+} & ComponentProps<'input'>
 
-export const TextField = ({ className, ...props }: Props) => {
-  return (
-    <>
-      <input className={clsx(s.root, className)} {...props} />
-    </>
-  )
-}
+export const TextField = forwardRef<HTMLInputElement, Props>(
+  ({ className, error, label, type, ...props }, ref) => {
+    return (
+      <div className={s.wrapper}>
+        {!!label && (
+          <Typography asChild className={s.label} variant={'regular_text_14'}>
+            <label htmlFor={label}>{label}</label>
+          </Typography>
+        )}
+        <input
+          className={clsx(s.input, !!error && s.errorInput, className)}
+          id={label}
+          ref={ref}
+          type={type}
+          {...props}
+        />
+        {!!error && (
+          <Typography className={s.error} variant={'regular_text_14'}>
+            {error}
+          </Typography>
+        )}
+      </div>
+    )
+  }
+)
