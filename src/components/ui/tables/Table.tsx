2909a3fb60e9b78fd4dd8395599706e4ba6ed7df
vustinchdev <vustinchdev@gmail.com>
2024-09-13 16:52:34 +0300

    INC-17 feat: add Table and story

--- src/components/ui/tables/Table.tsx
+++ src/components/ui/tables/Table.tsx
@@ -1,3 +1,37 @@
-export const Table = () => {
-  return <table />
-}
+import { ComponentPropsWithoutRef, ElementRef, forwardRef } from 'react'
+
+export const Table = forwardRef<ElementRef<'table'>, ComponentPropsWithoutRef<'table'>>(
+  ({ ...rest }, ref) => {
+    return <table {...rest} ref={ref} />
+  }
+)
+
+export const TableHead = forwardRef<ElementRef<'thead'>, ComponentPropsWithoutRef<'thead'>>(
+  ({ ...rest }, ref) => {
+    return <thead {...rest} ref={ref} />
+  }
+)
+
+export const TableBody = forwardRef<ElementRef<'tbody'>, ComponentPropsWithoutRef<'tbody'>>(
+  ({ ...rest }, ref) => {
+    return <tbody {...rest} ref={ref} />
+  }
+)
+
+export const TableRow = forwardRef<ElementRef<'tr'>, ComponentPropsWithoutRef<'tr'>>(
+  ({ ...rest }, ref) => {
+    return <tr {...rest} ref={ref} />
+  }
+)
+
+export const TableHeadCell = forwardRef<ElementRef<'th'>, ComponentPropsWithoutRef<'th'>>(
+  ({ ...rest }, ref) => {
+    return <th {...rest} ref={ref} />
+  }
+)
+
+export const TableBodyCell = forwardRef<ElementRef<'td'>, ComponentPropsWithoutRef<'td'>>(
+  ({ ...rest }, ref) => {
+    return <td {...rest} ref={ref} />
+  }
+)
