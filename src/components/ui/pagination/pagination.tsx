ba4b93c511ffe1dfeb1d133bffe08d0bbb81358e
VitaliyYurkevich <vitaliy.yurkevich@yandex.ru>
2024-09-16 12:07:01 +0300

    INC-15: pagination component

---
+++ src/components/ui/pagination/pagination.tsx
@@ -0,0 +1,55 @@
+import { FC, memo } from 'react'
+import { clsx } from 'clsx'
+import s from './pagination.module.scss'
+import { Typography } from '@/components/ui/typography'
+import { usePagination } from '@/components/ui/pagination/usePagination'
+import { NavigationBlock } from '@/components/ui/pagination/navigation-block/navigation-block'
+
+export type PaginationProps = {
+  totalCount?: number
+  currentPage: number
+  onPageChange: (newPage: number) => void
+  pageSize: number
+  onPageSizeChange: (newPageSize: number) => void
+  siblingCount?: number
+  className?: string
+}
+
+export const Pagination: FC<PaginationProps> = memo(
+  ({
+    totalCount = 10,
+    currentPage,
+    onPageChange,
+    pageSize,
+    siblingCount,
+    className,
+  }) => {
+    const classes = clsx(s.root, className)
+
+    const paginationRange = usePagination({
+      currentPage,
+      totalCount,
+      siblingCount,
+      pageSize,
+    })
+
+    if (currentPage === 0 || paginationRange.length < 2) {
+      return null
+    }
+
+    return (
+      <div className={classes}>
+        <NavigationBlock
+          paginationRange={paginationRange}
+          currentPage={currentPage}
+          onPageChange={onPageChange}
+        />
+        <div className={s.selectBlock}>
+          <Typography variant="regular_text_14">Show</Typography>
+          <select />
+          <Typography variant="regular_text_14">the page</Typography>
+        </div>
+      </div>
+    )
+  }
+)
