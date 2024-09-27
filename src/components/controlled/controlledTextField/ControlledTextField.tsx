16019d62e55f093884976d0f3400ad3d51ed4d73
puerchik <rumyantsevnn@yandex.ru>
2024-09-27 09:04:54 +0300

    INC-31 feat: add ControlledTextField component and stories

--- src/components/controlled/controlledTextField/ControlledTextField.tsx
+++ src/components/controlled/controlledTextField/ControlledTextField.tsx
@@ -1,3 +1,28 @@
-export const ControlledTextField = () => {
-  return <></>
+import { ComponentPropsWithoutRef } from 'react'
+import { Control, FieldPath, FieldValues, useController } from 'react-hook-form'
+
+import { TextField } from '@/components/ui/text-field'
+
+type Props<T extends FieldValues> = {
+  control: Control<T>
+  name: FieldPath<T>
+} & ComponentPropsWithoutRef<typeof TextField>
+
+export const ControlledTextField = <T extends FieldValues>({
+  control,
+  name,
+  ...props
+}: Props<T>) => {
+  const {
+    field: { onChange, value, ...field },
+  } = useController({
+    control,
+    name,
+  })
+
+  return (
+    <>
+      <TextField onChange={onChange} value={value} {...props} {...field} />
+    </>
+  )
 }
--- src/components/controlled/controlledTextField/controlledTextField.stories.tsx
+++ src/components/controlled/controlledTextField/controlledTextField.stories.tsx
@@ -1,15 +1,51 @@
+import { CSSProperties } from 'react'
+import { useForm } from 'react-hook-form'
+
 import { ControlledTextField } from '@/components/controlled/controlledTextField'
+import { Button } from '@/components/ui/button'
+import { Typography } from '@/components/ui/typography'
+import { DevTool } from '@hookform/devtools'
 import { Meta, StoryObj } from '@storybook/react'
 
 const meta = {
   component: ControlledTextField,
   tags: ['autodocs'],
-  title: 'Components/controlled/ControlledTextField',
+  title: 'Components/Controlled/ControlledTextField',
 } satisfies Meta<typeof ControlledTextField>
 
 export default meta
 type Story = StoryObj<typeof meta>
 
+type FormValues = {
+  firstName: string
+  lastName: string
+}
+
 export const Default: Story = {
-  args: {},
+  args: {} as any,
+
+  render: () => {
+    const { control, handleSubmit } = useForm<FormValues>()
+
+    const onSubmit = (data: FormValues) => {
+      alert(JSON.stringify(data))
+    }
+
+    const styles: CSSProperties = {
+      display: 'flex',
+      flexDirection: 'column',
+      maxWidth: '400px',
+      rowGap: '20px',
+    }
+
+    return (
+      <form onSubmit={handleSubmit(onSubmit)} style={styles}>
+        <DevTool control={control} />
+        <Typography>Form With Controlled Text Fields</Typography>
+        <ControlledTextField control={control} label={'First Name'} name={'firstName'} />
+        <ControlledTextField control={control} label={'Last Name'} name={'lastName'} />
+        <Button style={{ marginTop: '24px' }}>Send</Button>
+      </form>
+    )
+  },
 }
