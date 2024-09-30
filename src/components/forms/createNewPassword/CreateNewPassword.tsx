abb8b392a10e1e7385810df26ba653cf7ea21ed4
vustinchdev <vustinchdev@gmail.com>
2024-09-30 15:02:33 +0300

    INC-36 feat: add CreateNewPassword and story

--- src/components/forms/createNewPassword/CreateNewPassword.tsx
+++ src/components/forms/createNewPassword/CreateNewPassword.tsx
@@ -1,5 +1,47 @@
+import { useForm } from 'react-hook-form'
+
+import { FormTextField } from '@/components/controlled/formTextField'
+import { Button } from '@/components/ui/button'
 import { Card } from '@/components/ui/card'
+import { Typography } from '@/components/ui/typography'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { z } from 'zod'
+
+type FormValues = z.infer<typeof newPasswordSchema>
+
+const newPasswordSchema = z.object({
+  newPassword: z.string(),
+  passwordConfirmation: z.string(),
+})
 
 export const CreateNewPassword = () => {
-  return <Card></Card>
+  const { control, handleSubmit } = useForm<FormValues>({
+    defaultValues: { newPassword: '', passwordConfirmation: '' },
+    resolver: zodResolver(newPasswordSchema),
+  })
+
+  const handleCreateNewPassword = () => {}
+
+  return (
+    <Card>
+      <form onSubmit={handleSubmit(handleCreateNewPassword)}>
+        <FormTextField
+          control={control}
+          label={'New password'}
+          name={'newPassword'}
+          type={'password'}
+        />
+        <FormTextField
+          control={control}
+          label={'Password confirmation'}
+          name={'passwordConfirmation'}
+          type={'password'}
+        />
+        <Typography variant={'regular_text_14'}>
+          Your password must be between 6 and 20 characters
+        </Typography>
+        <Button>Create new password</Button>
+      </form>
+    </Card>
+  )
 }
