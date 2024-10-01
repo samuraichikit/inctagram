5fdac7b253c752fc3f3ac754499dfc87adcbb172
puerchik <rumyantsevnn@yandex.ru>
2024-10-01 10:27:26 +0300

    INC-33 feat: add SignIn component

---
+++ src/components/forms/signIn/SignIn.tsx
@@ -0,0 +1,67 @@
+import { useForm } from 'react-hook-form'
+
+import { GitHubIcon } from '@/assets/icons/GitHubIcon'
+import { GoogleIcon } from '@/assets/icons/GoogleIcon'
+import { FormTextField } from '@/components/controlled/formTextField'
+import { Button } from '@/components/ui/button'
+import { Card } from '@/components/ui/card'
+import { Typography } from '@/components/ui/typography'
+import clsx from 'clsx'
+
+import s from './signIn.module.scss'
+
+export const SignIn = () => {
+  const classNames = {
+    emailTextField: clsx(s.emailTextField, s.fullWidth, s.mainMargin),
+    forgotPassword: clsx(s.forgotPassword, s.mainMargin),
+    form: s.form,
+    iconWrapper: clsx(s.iconWrapper, s.mainMargin),
+    passwordTextField: clsx(s.passwordTextField, s.fullWidth),
+    signInButton: clsx(s.signInButton, s.fullWidth),
+    signUpQuestion: s.signUpQuestion,
+    title: s.title,
+    wrapper: s.wrapper,
+  }
+  const { control, handleSubmit } = useForm()
+
+  const onSubmitHandler = () => {}
+
+  return (
+    <Card className={classNames.wrapper}>
+      <form className={classNames.form} onSubmit={handleSubmit(onSubmitHandler)}>
+        <Typography asChild className={classNames.title} variant={'h1'}>
+          <h1>Sign In</h1>
+        </Typography>
+        <div className={classNames.iconWrapper}>
+          <GoogleIcon height={36} width={36} />
+          <GitHubIcon height={36} width={36} />
+        </div>
+
+        <FormTextField
+          className={classNames.emailTextField}
+          control={control}
+          label={'Email'}
+          name={'email'}
+          placeholder={'Epam@epam.com'}
+          type={'email'}
+        />
+        <FormTextField
+          className={classNames.passwordTextField}
+          control={control}
+          label={'Password'}
+          name={'password'}
+          placeholder={'Enter password'}
+          type={'password'}
+        />
+        <Typography asChild className={classNames.forgotPassword} variant={'regular_text_14'}>
+          <a href={'#'}>Forgot Password</a>
+        </Typography>
+        <Button className={classNames.signInButton}>Sign In</Button>
+        <Typography className={s.signUpQuestion} variant={'regular_text_16'}>
+          Don’t have an account?
+        </Typography>
+        <Button variant={'text'}>Sign Up</Button>
+      </form>
+    </Card>
+  )
+}
