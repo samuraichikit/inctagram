bba9b74c4eaa40f970908ffcb9d1f8269ef0df6b
vustinchdev <vustinchdev@gmail.com>
2024-10-08 10:42:11 +0300

    INC-38 feat: add store and baseApi

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,18 @@
 import type { AppProps } from 'next/app'
 
+import { Provider } from 'react-redux'
+
+import { wrapper } from '@/app/store'
+
 import './../styles/index.scss'
 import '@fontsource-variable/inter'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function App({ Component, ...rest }: AppProps) {
+  const { props, store } = wrapper.useWrappedStore(rest)
+
+  return (
+    <Provider store={store}>
+      <Component {...props.pageProps} />
+    </Provider>
+  )
 }
