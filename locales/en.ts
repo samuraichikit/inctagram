9bed8e199bf59ee33052e9d1f239ce02ede2208d
e-nie <eniehaus111@gmail.com>
2024-10-12 23:20:42 +0200

    add en.ts, ru.ts

--- locales/en.ts
+++ locales/en.ts
@@ -1,11 +1,5 @@
 import { LocaleType } from './ru'
 
 export const en: LocaleType = {
-  charactersPage: {
-    title: 'Character page',
-  },
-  locationsPage: {
-    title: 'Location page',
-  },
   test: 'Lorem Ipsum is simply dummy text of the printing',
 }
--- locales/ru.ts
+++ locales/ru.ts
@@ -1,10 +1,4 @@
 export const ru = {
-  charactersPage: {
-    title: 'Страница с персонажами',
-  },
-  locationsPage: {
-    title: 'Страница с локациями',
-  },
   test: 'Lorem Ipsum - это текст-"рыба", часто используемый в печати',
 }
 
