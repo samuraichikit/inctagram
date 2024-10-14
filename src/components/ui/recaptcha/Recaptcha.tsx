ede8952b22d36c63bc03699466361f343a59dd0c
ALEXANDR Kovalev <yakovalev1@mail.ru>
2024-10-15 02:59:44 +0300

    INC-30 feat: added multilingual and wrapped to forwardRef

--- src/components/ui/recaptcha/Recaptcha.tsx
+++ src/components/ui/recaptcha/Recaptcha.tsx
@@ -1,27 +1,37 @@
-import React, { ComponentPropsWithoutRef } from 'react'
+import React, { ComponentPropsWithoutRef, ElementRef, forwardRef } from 'react'
 import ReCAPTCHA from 'react-google-recaptcha'
 
 import { Typography } from '@/components/ui/typography'
+import { useRouter } from 'next/router'
 
 import s from './recaptcha.module.scss'
 
 type Props = {
   errorMessage?: string
 } & ComponentPropsWithoutRef<typeof ReCAPTCHA>
-export const Recaptcha = ({ errorMessage, style, ...props }: Props) => {
-  return (
-    <div className={s.reCaptcha}>
-      <ReCAPTCHA
-        {...props}
-        style={{
-          height: '100%',
-          maxHeight: '84px',
-          maxWidth: '300px',
-          width: '100%',
-          ...style,
-        }}
-      />
-      {errorMessage && <Typography variant={'error'}>{errorMessage}</Typography>}
-    </div>
-  )
-}
+
+export const Recaptcha = forwardRef<ElementRef<typeof ReCAPTCHA>, Props>(
+  ({ errorMessage, hl, style, ...props }, ref) => {
+    const router = useRouter()
+
+    const lang = router.locale ?? 'ru'
+
+    return (
+      <div className={s.reCaptcha}>
+        <ReCAPTCHA
+          hl={hl ?? lang}
+          ref={ref}
+          {...props}
+          style={{
+            height: '100%',
+            maxHeight: '84px',
+            maxWidth: '300px',
+            width: '100%',
+            ...style,
+          }}
+        />
+        {errorMessage && <Typography variant={'error'}>{errorMessage}</Typography>}
+      </div>
+    )
+  }
+)
