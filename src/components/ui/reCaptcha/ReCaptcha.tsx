7ccad75e54d2eb945ba72d6bced016188f72e9ef
ALEXANDR Kovalev <yakovalev1@mail.ru>
2024-10-15 03:40:11 +0300

    INC-30 refactor: edit recaptcha styles

--- src/components/ui/reCaptcha/ReCaptcha.tsx
+++ src/components/ui/reCaptcha/ReCaptcha.tsx
@@ -2,22 +2,24 @@ import React, { ComponentPropsWithoutRef, ElementRef, forwardRef } from 'react'
 import ReCAPTCHA from 'react-google-recaptcha'
 
 import { Typography } from '@/components/ui/typography'
+import clsx from 'clsx'
 import { useRouter } from 'next/router'
 
-import s from './recaptcha.module.scss'
+import s from './reCaptcha.module.scss'
 
 type Props = {
   errorMessage?: string
 } & ComponentPropsWithoutRef<typeof ReCAPTCHA>
 
-export const Recaptcha = forwardRef<ElementRef<typeof ReCAPTCHA>, Props>(
+export const ReCaptcha = forwardRef<ElementRef<typeof ReCAPTCHA>, Props>(
   ({ errorMessage, hl, style, ...props }, ref) => {
     const router = useRouter()
 
     const lang = router.locale ?? 'ru'
+    const wrapperClass = clsx(s.reCaptcha, errorMessage && s.error)
 
     return (
-      <div className={s.reCaptcha}>
+      <div className={wrapperClass}>
         <ReCAPTCHA
           hl={hl ?? lang}
           ref={ref}
