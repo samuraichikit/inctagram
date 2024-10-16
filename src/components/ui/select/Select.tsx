76c0f600ab2b91265cb8aacacbedd86b66682f5d
vustinchdev <vustinchdev@gmail.com>
2024-10-16 14:54:54 +0300

    INC-41 feat: add ref support to Select and SelectItem components

--- src/components/ui/select/Select.tsx
+++ src/components/ui/select/Select.tsx
@@ -1,4 +1,4 @@
-import { ComponentPropsWithoutRef, ReactNode, useId } from 'react'
+import { ComponentPropsWithoutRef, ElementRef, ReactNode, forwardRef, useId } from 'react'
 
 import { DropDownArrowIcon } from '@/assets/icons/DropDownArrow'
 import { Typography } from '@/components/ui/typography'
@@ -14,32 +14,34 @@ export type SelectProps = {
   small?: boolean
 } & ComponentPropsWithoutRef<typeof SelectRadix.Root>
 
-export const Select = ({ children, id, label, placeholder, small, ...rest }: SelectProps) => {
-  const generatedId = useId()
-  const idToUse = id ?? generatedId
+export const Select = forwardRef<ElementRef<typeof SelectRadix.Root>, SelectProps>(
+  ({ children, id, label, placeholder, small, ...rest }, ref) => {
+    const generatedId = useId()
+    const idToUse = id ?? generatedId
 
-  return (
-    <div className={s.wrapper}>
-      {!!label && (
-        <Typography asChild className={s.label} variant={'regular_text_14'}>
-          <label htmlFor={idToUse}>{label}</label>
-        </Typography>
-      )}
-      <SelectRadix.Root {...rest}>
-        <SelectRadix.Trigger className={clsx(s.trigger, small && s.small)} id={idToUse}>
-          <SelectRadix.Value placeholder={placeholder} />
-          <DropDownArrowIcon className={s.dropDownArrowIcon} height={24} width={24} />
-        </SelectRadix.Trigger>
-        <SelectRadix.Portal>
-          <SelectRadix.Content
-            className={clsx(s.content, small && s.small)}
-            collisionPadding={0}
-            position={'popper'}
-          >
-            <SelectRadix.Viewport className={s.viewport}>{children}</SelectRadix.Viewport>
-          </SelectRadix.Content>
-        </SelectRadix.Portal>
-      </SelectRadix.Root>
-    </div>
-  )
-}
+    return (
+      <div className={s.wrapper}>
+        {!!label && (
+          <Typography asChild className={s.label} variant={'regular_text_14'}>
+            <label htmlFor={idToUse}>{label}</label>
+          </Typography>
+        )}
+        <SelectRadix.Root {...rest}>
+          <SelectRadix.Trigger className={clsx(s.trigger, small && s.small)} id={idToUse} ref={ref}>
+            <SelectRadix.Value placeholder={placeholder} />
+            <DropDownArrowIcon className={s.dropDownArrowIcon} height={24} width={24} />
+          </SelectRadix.Trigger>
+          <SelectRadix.Portal>
+            <SelectRadix.Content
+              className={clsx(s.content, small && s.small)}
+              collisionPadding={0}
+              position={'popper'}
+            >
+              <SelectRadix.Viewport className={s.viewport}>{children}</SelectRadix.Viewport>
+            </SelectRadix.Content>
+          </SelectRadix.Portal>
+        </SelectRadix.Root>
+      </div>
+    )
+  }
+)
--- src/components/ui/select/selectItem/SelectItem.tsx
+++ src/components/ui/select/selectItem/SelectItem.tsx
@@ -1,4 +1,4 @@
-import { ComponentPropsWithoutRef } from 'react'
+import { ComponentPropsWithoutRef, ElementRef, forwardRef } from 'react'
 
 import * as SelectRadix from '@radix-ui/react-select'
 import clsx from 'clsx'
@@ -9,10 +9,12 @@ type SelectItemProps = {
   small?: boolean
 } & ComponentPropsWithoutRef<typeof SelectRadix.Item>
 
-export const SelectItem = ({ children, className, small, ...rest }: SelectItemProps) => {
-  return (
-    <SelectRadix.Item className={clsx(s.item, small && s.small, className)} {...rest}>
-      <SelectRadix.ItemText>{children}</SelectRadix.ItemText>
-    </SelectRadix.Item>
-  )
-}
+export const SelectItem = forwardRef<ElementRef<typeof SelectRadix.Item>, SelectItemProps>(
+  ({ children, className, small, ...rest }, ref) => {
+    return (
+      <SelectRadix.Item className={clsx(s.item, small && s.small, className)} {...rest} ref={ref}>
+        <SelectRadix.ItemText>{children}</SelectRadix.ItemText>
+      </SelectRadix.Item>
+    )
+  }
+)
