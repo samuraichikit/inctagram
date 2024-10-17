ebd0d1117388ec316dc01e2100f201d8866dca35
vustinchdev <vustinchdev@gmail.com>
2024-10-17 21:17:27 +0300

    INC-41 feat: replace native select with Select in LangSelect

--- src/components/langSelect/LangSelect.tsx
+++ src/components/langSelect/LangSelect.tsx
@@ -1,37 +1,51 @@
-import { ChangeEvent } from 'react'
-
+import Image, { StaticImageData } from 'next/image'
 import { useRouter } from 'next/router'
 
+import s from './langSelect.module.scss'
+
+import { Select } from '../ui/select'
+import { SelectItem } from '../ui/select/selectItem'
+import { Typography } from '../ui/typography'
+import flagRussia from './../../../public/flagRussia.png'
+import flagUnitedKingdom from './../../../public/flagUnitedKingdom.png'
+
+type Locales = 'en' | 'ru'
+
+type LanguageDetails = {
+  language: string
+  src: StaticImageData
+}
+
 export const LangSelect = () => {
+  const classNames = {
+    selectItem: s.selectItem,
+  }
   const { asPath, locale, locales, pathname, push, query } = useRouter()
 
-  const changeLangHandler = (event: ChangeEvent<HTMLSelectElement>) => {
-    const locale = event.currentTarget.value
+  const languagesDetails: Record<Locales, LanguageDetails> = {
+    en: { language: 'English', src: flagUnitedKingdom },
+    ru: { language: 'Русский', src: flagRussia },
+  }
 
-    push({ pathname, query }, asPath, { locale })
+  const changeLangHandler = (value: string) => {
+    push({ pathname, query }, asPath, { locale: value })
   }
 
   return (
-    <div style={{ padding: '20px' }}>
-      <select
-        defaultValue={locale}
-        onChange={changeLangHandler}
-        style={{
-          background: 'black',
-          marginRight: '20px',
-          position: 'absolute',
-          right: '0',
-          top: '20',
-        }}
-      >
-        {locales?.map(l => {
-          return (
-            <option key={l} value={l}>
-              {l}
-            </option>
-          )
-        })}
-      </select>
-    </div>
+    <Select onValueChange={changeLangHandler} value={locale}>
+      {locales?.map(l => {
+        const languageParams = languagesDetails[l as Locales]
+        const { language, src } = languageParams
+
+        return (
+          <SelectItem key={l} value={l}>
+            <div className={classNames.selectItem}>
+              <Image alt={l} height={20} src={src} width={20} />
+              <Typography variant={'regular_text_16'}>{language}</Typography>
+            </div>
+          </SelectItem>
+        )
+      })}
+    </Select>
   )
 }
