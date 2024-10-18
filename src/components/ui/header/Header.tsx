6ff3c44c81de5cf59c10a454a8795924345a40d5
vustinchdev <vustinchdev@gmail.com>
2024-10-18 22:32:53 +0300

    INC-41 feat: add styles for Header

--- src/components/ui/header/Header.tsx
+++ src/components/ui/header/Header.tsx
@@ -1,21 +1,34 @@
 import { LangSelect } from '@/components/langSelect/LangSelect'
 import Link from 'next/link'
 
+import s from './header.module.scss'
+
 import { Button } from '../button'
 import { Typography } from '../typography'
 
 export const Header = () => {
+  const classNames = {
+    buttonsContainer: s.buttonsContainer,
+    container: s.container,
+    header: s.header,
+    navContainer: s.navContainer,
+  }
+
   return (
-    <header>
-      <div>
+    <header className={classNames.header}>
+      <div className={classNames.container}>
         <Typography variant={'large'}>Inctagram</Typography>
-        <LangSelect />
-        <Button asChild variant={'text'}>
-          <Link href={'auth/signIn'}>Log in</Link>
-        </Button>
-        <Button asChild>
-          <Link href={'auth/signUp'}>Sign up</Link>
-        </Button>
+        <div className={classNames.navContainer}>
+          <LangSelect />
+          <div className={classNames.buttonsContainer}>
+            <Button asChild variant={'text'}>
+              <Link href={'auth/signIn'}>Log in</Link>
+            </Button>
+            <Button asChild>
+              <Link href={'auth/signUp'}>Sign up</Link>
+            </Button>
+          </div>
+        </div>
       </div>
     </header>
   )
