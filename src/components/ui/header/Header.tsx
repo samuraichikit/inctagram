3b781fb3769002079bf79100d555a382ec2c428e
vustinchdev <vustinchdev@gmail.com>
2024-10-18 15:04:28 +0300

    INC-41 feat: add Header and story

--- src/components/ui/header/Header.tsx
+++ src/components/ui/header/Header.tsx
@@ -1,3 +1,22 @@
+import { LangSelect } from '@/components/langSelect/LangSelect'
+import Link from 'next/link'
+
+import { Button } from '../button'
+import { Typography } from '../typography'
+
 export const Header = () => {
-  return <header></header>
+  return (
+    <header>
+      <div>
+        <Typography variant={'large'}>Inctagram</Typography>
+        <LangSelect />
+        <Button asChild variant={'text'}>
+          <Link href={'auth/signIn'}>Log in</Link>
+        </Button>
+        <Button asChild>
+          <Link href={'auth/signUp'}>Sign up</Link>
+        </Button>
+      </div>
+    </header>
+  )
 }
