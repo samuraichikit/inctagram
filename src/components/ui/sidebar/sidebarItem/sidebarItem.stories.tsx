4f21b8f758a9b71bf0db1c89922a5804dbd54556
vustinchdev <vustinchdev@gmail.com>
2024-10-30 22:20:45 +0300

    INC-54 feat: add story for SidebarItem

---
+++ src/components/ui/sidebar/sidebarItem/sidebarItem.stories.tsx
@@ -0,0 +1,27 @@
+import type { Meta, StoryObj } from '@storybook/react'
+
+import { HomeOutlineIcon } from '@/assets/icons/HomeOutline'
+
+import { Typography } from '../../typography'
+import { SidebarItem } from './SidebarItem'
+
+const meta = {
+  argTypes: {},
+  component: SidebarItem,
+  tags: ['autodocs'],
+  title: 'Components/Sidebar/SidebarItem',
+} satisfies Meta<typeof SidebarItem>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const SidebarItemStory: Story = {
+  args: {
+    children: (
+      <>
+        <HomeOutlineIcon /> <Typography variant={'medium_text_14'}>Home</Typography>
+      </>
+    ),
+    href: '#',
+  },
+}
