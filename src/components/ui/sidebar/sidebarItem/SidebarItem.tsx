73b33e968318855b387c29e13111b90f006b0455
vustinchdev <vustinchdev@gmail.com>
2024-10-30 19:56:54 +0300

    INC-54 feat: add SidebarItem

--- src/components/ui/sidebar/sidebarItem/SidebarItem.tsx
+++ src/components/ui/sidebar/sidebarItem/SidebarItem.tsx
@@ -1,3 +1,16 @@
-export const SidebarItem = () => {
-  return <li></li>
+import { ReactNode } from 'react'
+
+import Link from 'next/link'
+
+type Props = {
+  children: ReactNode
+  href: string
+}
+
+export const SidebarItem = ({ children, href }: Props) => {
+  return (
+    <li>
+      <Link href={href}>{children}</Link>
+    </li>
+  )
 }
