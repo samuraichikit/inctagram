5495a9abaae6a06ea67945ddafb426395ed952fe
vustinchdev <vustinchdev@gmail.com>
2024-11-01 13:02:51 +0300

    INC-54 feat: add styles for MainSidebar

--- src/components/ui/sidebar/mainSidebar/MainSidebar.tsx
+++ src/components/ui/sidebar/mainSidebar/MainSidebar.tsx
@@ -6,44 +6,54 @@ import { PlusSquareOutlineIcon } from '@/assets/icons/PlusSquareOutline'
 import { SearchIcon } from '@/assets/icons/SearchIcon'
 import { TrendingUpOutlineIcon } from '@/assets/icons/TrendingUpOutline'
 
+import s from './mainSidebar.module.scss'
+
 import { Sidebar } from '..'
 import { LogoutButton } from '../logoutButton'
 import { SidebarItem } from '../sidebarItem'
 
 export const MainSidebar = () => {
+  const classNames = {
+    mainItems: s.mainItems,
+    secondaryItems: s.secondaryItems,
+    sidebar: s.sidebar,
+  }
+
   return (
-    <Sidebar>
-      <ul>
-        <SidebarItem href={'#'}>
-          <HomeOutlineIcon />
-          Home
-        </SidebarItem>
-        <SidebarItem href={'#'}>
-          <PlusSquareOutlineIcon />
-          Create
-        </SidebarItem>
-        <SidebarItem href={'#'}>
-          <MyProfileIcon />
-          MyProfile
-        </SidebarItem>
-        <SidebarItem href={'#'}>
-          <MessengerIcon />
-          Messenger
-        </SidebarItem>
-        <SidebarItem href={'#'}>
-          <SearchIcon className={''} />
-          Seach
-        </SidebarItem>
-      </ul>
-      <ul>
-        <SidebarItem href={'#'}>
-          <TrendingUpOutlineIcon /> Statistics
-        </SidebarItem>
-        <SidebarItem href={'#'}>
-          <BookmarkOutlineIcon />
-          Favorites
-        </SidebarItem>
-      </ul>
+    <Sidebar className={classNames.sidebar}>
+      <div>
+        <ul className={classNames.mainItems}>
+          <SidebarItem href={'#'}>
+            <HomeOutlineIcon />
+            Home
+          </SidebarItem>
+          <SidebarItem href={'#'}>
+            <PlusSquareOutlineIcon />
+            Create
+          </SidebarItem>
+          <SidebarItem href={'#'}>
+            <MyProfileIcon />
+            MyProfile
+          </SidebarItem>
+          <SidebarItem href={'#'}>
+            <MessengerIcon />
+            Messenger
+          </SidebarItem>
+          <SidebarItem href={'#'}>
+            <SearchIcon className={''} />
+            Seach
+          </SidebarItem>
+        </ul>
+        <ul className={classNames.secondaryItems}>
+          <SidebarItem href={'#'}>
+            <TrendingUpOutlineIcon /> Statistics
+          </SidebarItem>
+          <SidebarItem href={'#'}>
+            <BookmarkOutlineIcon />
+            Favorites
+          </SidebarItem>
+        </ul>
+      </div>
       <LogoutButton />
     </Sidebar>
   )
