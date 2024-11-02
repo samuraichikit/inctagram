ea7f00af56eabe71301440c5995e1741c305e086
puerchik <rumyantsevnn@yandex.ru>
2024-11-02 09:47:37 +0300

    INC-64 fix: fix Terms of Service and Privacy Policy styles

--- src/components/ui/layout/BaseLayout.tsx
+++ src/components/ui/layout/BaseLayout.tsx
@@ -4,17 +4,22 @@ import { useAuth } from '@/common/hooks/useAuth'
 import { useMeQuery } from '@/services/auth/authService'
 import clsx from 'clsx'
 import { NextPage } from 'next'
+import { usePathname } from 'next/navigation'
 
 import s from './baseLayout.module.scss'
 
 import { Header } from '../header'
 
 export const BaseLayout: NextPage<PropsWithChildren> = ({ children }) => {
-  const classNames = {
-    main: s.main,
-  }
-
   const { isAuth } = useAuth()
+  const path = usePathname()
+
+  const classNames = {
+    main: clsx(
+      s.main,
+      (path === '/auth/privacyPolicy' || path === '/auth/termsOfService') && s.privacyPolicy
+    ),
+  }
 
   return (
     <>
