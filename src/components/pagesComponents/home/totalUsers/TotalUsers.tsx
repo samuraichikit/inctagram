0e5dcd8db38a300228ebe3926b6f2dcb9001c0ed
vustinchdev <vustinchdev@gmail.com>
2024-11-09 14:44:31 +0300

    INC-76 feat: add TotlalUsers

--- src/components/pagesComponents/home/totalUsers/TotalUsers.tsx
+++ src/components/pagesComponents/home/totalUsers/TotalUsers.tsx
@@ -1,5 +1,26 @@
 import { Card } from '@/components/ui/card'
+import { Typography } from '@/components/ui/typography'
 
-export const TotalUsers = () => {
-  return <Card></Card>
+type Props = {
+  totalUsers: number
+}
+
+export const TotalUsers = ({ totalUsers }: Props) => {
+  const DIGITS_COUNT = 6
+  const totalUsersFormatted = totalUsers.toString().padStart(DIGITS_COUNT, '0').split('')
+
+  return (
+    <Card>
+      <Typography asChild variant={'h2'}>
+        <h2>Registered users:</h2>
+      </Typography>
+      <Card>
+        <Typography variant={'h2'}>
+          {totalUsersFormatted.map((digit, index) => {
+            return <span key={index}>{digit}</span>
+          })}
+        </Typography>
+      </Card>
+    </Card>
+  )
 }
