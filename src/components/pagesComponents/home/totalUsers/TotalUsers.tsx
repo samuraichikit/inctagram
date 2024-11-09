03093b07b782f25edd43d65c63bccf8307cd6fe7
vustinchdev <vustinchdev@gmail.com>
2024-11-09 16:05:00 +0300

    INC-76 feat: add styles for TotalUsers

--- src/components/pagesComponents/home/totalUsers/TotalUsers.tsx
+++ src/components/pagesComponents/home/totalUsers/TotalUsers.tsx
@@ -1,25 +1,35 @@
 import { Card } from '@/components/ui/card'
 import { Typography } from '@/components/ui/typography'
 
+import s from './totalUsers.module.scss'
+
 type Props = {
   totalUsers: number
 }
 
 export const TotalUsers = ({ totalUsers }: Props) => {
+  const classNames = {
+    container: s.container,
+    digit: s.digit,
+    digitContainer: s.digitContainer,
+    totalUsersCard: s.totalUsersCard,
+  }
   const DIGITS_COUNT = 6
   const totalUsersFormatted = totalUsers.toString().padStart(DIGITS_COUNT, '0').split('')
 
   return (
-    <Card>
+    <Card className={classNames.container}>
       <Typography asChild variant={'h2'}>
         <h2>Registered users:</h2>
       </Typography>
-      <Card>
-        <Typography variant={'h2'}>
-          {totalUsersFormatted.map((digit, index) => {
-            return <span key={index}>{digit}</span>
-          })}
-        </Typography>
+      <Card className={classNames.totalUsersCard}>
+        {totalUsersFormatted.map((digit, index) => {
+          return (
+            <Typography className={classNames.digitContainer} key={index} variant={'h2'}>
+              <span className={classNames.digit}>{digit}</span>
+            </Typography>
+          )
+        })}
       </Card>
     </Card>
   )
