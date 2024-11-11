3fe52602391459f21175cbe8949c4301286af652
puerchik <rumyantsevnn@yandex.ru>
2024-11-11 10:14:35 +0300

    INC-51 feat: add profile settings bar

--- src/components/ui/profileSettingsBar/ProfileSettingsBar.tsx
+++ src/components/ui/profileSettingsBar/ProfileSettingsBar.tsx
@@ -1,29 +1,55 @@
 import { useTranslation } from '@/common/hooks/useTranslation'
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
+import { usePathname } from 'next/navigation'
+import { useRouter } from 'next/router'
 
 import s from './profileSettingsBar.module.scss'
 
 export const ProfileSettingsBar = () => {
   const { t } = useTranslation()
+
+  const path = usePathname()
+  const router = useRouter()
   const { accountManagement, devices, generalInformation, myPayments } = t.profile.settings
-  const settings = [generalInformation, devices, accountManagement, myPayments]
-  const paths = ['general', 'devices', 'management', 'payments']
+  const userId = path?.split('/').reverse()[0]
+  const currentSetting = path?.split('/').reverse()[1]
+
+  console.log(currentSetting)
 
   return (
     <>
-      <Tabs className={s.settingsList}>
+      <Tabs className={s.settingsList} defaultValue={generalInformation}>
         <TabsList>
-          {settings.map((el, i) => (
-            <TabsTrigger key={i} value={el}>
-              {el}
-            </TabsTrigger>
-          ))}
+          <TabsTrigger
+            onClick={() => router.push(`/profile/settings/general/${userId}`)}
+            value={generalInformation}
+          >
+            {generalInformation}
+          </TabsTrigger>
+          <TabsTrigger
+            onClick={() => router.push(`/profile/settings/devices/${userId}`)}
+            value={devices}
+          >
+            {devices}
+          </TabsTrigger>
+          <TabsTrigger
+            onClick={() => router.push(`/profile/settings/management/${userId}`)}
+            value={accountManagement}
+          >
+            {accountManagement}
+          </TabsTrigger>
+          <TabsTrigger
+            onClick={() => router.push(`/profile/settings/payments/${userId}`)}
+            value={myPayments}
+          >
+            {myPayments}
+          </TabsTrigger>
         </TabsList>
-        {settings.map((el, i) => (
-          <TabsContent key={i} value={el}>
-            {el}
-          </TabsContent>
-        ))}
+
+        <TabsContent value={generalInformation}>{generalInformation}</TabsContent>
+        <TabsContent value={devices}>{devices}</TabsContent>
+        <TabsContent value={accountManagement}>{accountManagement}</TabsContent>
+        <TabsContent value={myPayments}>{myPayments}</TabsContent>
       </Tabs>
     </>
   )
