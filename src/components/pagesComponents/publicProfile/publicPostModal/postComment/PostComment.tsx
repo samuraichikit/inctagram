93a4b8ecdf295956a86a79703297739ab381ae5a
vustinchdev <vustinchdev@gmail.com>
2024-11-17 16:02:54 +0300

    INC-76 feat: add styles for PostComment

--- src/components/pagesComponents/publicProfile/publicPostModal/postComment/PostComment.tsx
+++ src/components/pagesComponents/publicProfile/publicPostModal/postComment/PostComment.tsx
@@ -3,6 +3,8 @@ import TimeAgo from 'react-timeago'
 import { Avatar } from '@/components/ui/avatar'
 import { Typography } from '@/components/ui/typography'
 
+import s from './postComment.module.scss'
+
 type Props = {
   answerCount: number
   avatarSrc: string
@@ -12,17 +14,36 @@ type Props = {
 }
 
 export const PostComment = ({ answerCount, avatarSrc, comment, createdAt, userName }: Props) => {
+  const classNames = {
+    answers: s.answers,
+    comment: s.comment,
+    container: s.container,
+    content: s.content,
+    timeAgo: s.timeAgo,
+    userName: s.userName,
+  }
   const displayViewAnswer = answerCount > 0
 
   return (
-    <>
+    <div className={classNames.container}>
       <Avatar src={avatarSrc} />
-      <Typography variant={'bold_text_14'}>{userName}</Typography>
-      <Typography variant={'regular_text_14'}>{comment}</Typography>
-      <TimeAgo date={createdAt} />
-      {displayViewAnswer && (
-        <Typography variant={'semi-bold_small_text'}>{`View Answers (${answerCount})`}</Typography>
-      )}
-    </>
+      <div className={classNames.content}>
+        <Typography className={classNames.userName} variant={'bold_text_14'}>
+          {userName}
+        </Typography>{' '}
+        <Typography className={classNames.comment} variant={'regular_text_14'}>
+          {comment}
+        </Typography>
+        <Typography className={classNames.timeAgo} variant={'small_text'}>
+          <TimeAgo date={createdAt} />
+        </Typography>
+        {displayViewAnswer && (
+          <Typography
+            className={classNames.answers}
+            variant={'semi-bold_small_text'}
+          >{`View Answers (${answerCount})`}</Typography>
+        )}
+      </div>
+    </div>
   )
 }
