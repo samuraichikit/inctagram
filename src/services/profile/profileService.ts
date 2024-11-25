9e3ddc907f4d0b8b87e5e25af8f49a09cb99746b
VitaliyYurkevich <vitaliy.yurkevich@yandex.ru>
2024-11-25 09:11:35 +0300

    add Photo add and delete logic

--- src/services/profile/profileService.ts
+++ src/services/profile/profileService.ts
@@ -7,21 +7,51 @@ import {
 
 const profileService = baseApi.injectEndpoints({
   endpoints: builder => ({
+    deleteAvatar: builder.mutation<void, void>({
+      invalidatesTags: ['Me', 'Profile'],
+      query: () => ({
+        method: 'DELETE',
+        url: 'v1/users/profile/avatar',
+      }),
+    }),
     getProfile: builder.query<GetProfileResponse, void>({
       providesTags: ['Me'],
       query: () => ({ url: 'v1/users/profile' }),
     }),
+
     getProfileWithPosts: builder.query<GetProfileWithPostsResponse, string>({
       providesTags: ['Me'],
       query: userId => ({ url: `v1/users/${userId}` }),
     }),
-
     updateProfile: builder.mutation<void, UpdateProfile>({
       invalidatesTags: ['Me'],
       query: body => ({ body, method: 'PUT', url: 'v1/users/profile' }),
     }),
+    uploadAvatar: builder.mutation<void, AvatarFile>({
+      invalidatesTags: ['Me'],
+      query: ({ file }) => {
+        const formData = new FormData()
+
+        formData.append('file', file)
+
+        return {
+          body: formData,
+          method: 'POST',
+          url: 'v1/users/profile/avatar',
+        }
+      },
+    }),
   }),
 })
 
-export const { useGetProfileQuery, useGetProfileWithPostsQuery, useUpdateProfileMutation } =
-  profileService
+export const {
+  useDeleteAvatarMutation,
+  useGetProfileQuery,
+  useGetProfileWithPostsQuery,
+  useUpdateProfileMutation,
+  useUploadAvatarMutation,
+} = profileService
+
+type AvatarFile = {
+  file: File
+}
