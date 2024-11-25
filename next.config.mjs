5f4c8ec6da5ccea5f1d03806cd7af6187f2d2586
VitaliyYurkevich <vitaliy.yurkevich@yandex.ru>
2024-11-25 09:09:09 +0300

    add images in next config

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,15 @@ const nextConfig = {
         defaultLocale: "ru",
         locales: ["en", "ru"],
     },
+    images: {
+        domains: ['staging-it-incubator.s3.eu-central-1.amazonaws.com'],
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'placehold.co',
+            },
+        ],
+    },
     reactStrictMode: true,
 
 };
