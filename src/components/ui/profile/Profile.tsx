67cc2186ea7dfb56a1577ed7d9886afc6a6aafea
vustinchdev <vustinchdev@gmail.com>
2024-12-15 09:49:07 +0300

    INC-83 feat: add display for information about me

--- src/components/ui/profile/Profile.tsx
+++ src/components/ui/profile/Profile.tsx
@@ -100,11 +100,7 @@ export const Profile = ({ comments, post }: Props) => {
             </ul>
           </div>
           <div>
-            <Typography className={s.aboutMe}>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-              incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-              exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-            </Typography>
+            <Typography className={s.aboutMe}>{profileInfo?.aboutMe}</Typography>
           </div>
         </div>
       </div>
