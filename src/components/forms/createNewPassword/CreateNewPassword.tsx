07318f92267cfd8dc70f18f52dcb2dd730860ab9
nikolasmushroom <dashagribok0905@gmail.com>
2024-12-25 23:59:37 +0300

    INC-27 feat: moved logic to custom hook. Add recovery-password logic

--- src/components/forms/createNewPassword/CreateNewPassword.tsx
+++ src/components/forms/createNewPassword/CreateNewPassword.tsx
@@ -1,20 +1,12 @@
-import { useForm } from 'react-hook-form'
-
-import { useTranslation } from '@/common/hooks/useTranslation'
-import { newPasswordSchema } from '@/common/schemas'
 import { FormTextField } from '@/components/controlled/formTextField'
+import { useCreateNewPassword } from '@/components/forms/createNewPassword/useCreateNewPassword'
 import { Button } from '@/components/ui/button'
 import { Card } from '@/components/ui/card'
 import { Typography } from '@/components/ui/typography'
-import { zodResolver } from '@hookform/resolvers/zod'
-import { z } from 'zod'
 
 import s from './createNewPassword.module.scss'
 
-type FormValues = z.infer<ReturnType<typeof newPasswordSchema>>
-
 export const CreateNewPassword = () => {
-  const { t } = useTranslation()
   const classNames = {
     buttonSubmit: s.buttonSubmit,
     card: s.card,
@@ -23,39 +15,41 @@ export const CreateNewPassword = () => {
     newPasswordInput: s.newPasswordInput,
     title: s.title,
   }
-
-  const { control, handleSubmit } = useForm<FormValues>({
-    defaultValues: { newPassword: '', passwordConfirmation: '' },
-    mode: 'onBlur',
-    resolver: zodResolver(newPasswordSchema(t)),
-  })
-
-  const handleCreateNewPassword = () => {}
+  const { control, handleCreateNewPassword, handleSubmit, isLoading, isValid, t } =
+    useCreateNewPassword()
 
   return (
-    <Card className={classNames.card}>
-      <form className={classNames.form} onSubmit={handleSubmit(handleCreateNewPassword)}>
-        <Typography asChild className={classNames.title} variant={'h1'}>
-          <h1>{t.passwordForm.createNewPassword}</h1>
-        </Typography>
-        <FormTextField
-          className={classNames.newPasswordInput}
-          control={control}
-          label={t.passwordForm.newPassword}
-          name={'newPassword'}
-          type={'password'}
-        />
-        <FormTextField
-          control={control}
-          label={t.passwordForm.passwordConfirmation}
-          name={'passwordConfirmation'}
-          type={'password'}
-        />
-        <Typography className={classNames.instructions} variant={'regular_text_14'}>
-          {t.passwordForm.passwordLengthMsg}
-        </Typography>
-        <Button className={classNames.buttonSubmit}>{t.passwordForm.createNewPassBtn}</Button>
-      </form>
-    </Card>
+    <>
+      {isLoading ? (
+        <div>Loading...</div>
+      ) : (
+        <Card className={classNames.card}>
+          <form className={classNames.form} onSubmit={handleSubmit(handleCreateNewPassword)}>
+            <Typography asChild className={classNames.title} variant={'h1'}>
+              <h1>{t.passwordForm.createNewPassword}</h1>
+            </Typography>
+            <FormTextField
+              className={classNames.newPasswordInput}
+              control={control}
+              label={t.passwordForm.newPassword}
+              name={'newPassword'}
+              type={'password'}
+            />
+            <FormTextField
+              control={control}
+              label={t.passwordForm.passwordConfirmation}
+              name={'passwordConfirmation'}
+              type={'password'}
+            />
+            <Typography className={classNames.instructions} variant={'regular_text_14'}>
+              {t.passwordForm.passwordLengthMsg}
+            </Typography>
+            <Button className={classNames.buttonSubmit} disabled={!isValid}>
+              {t.passwordForm.createNewPassBtn}
+            </Button>
+          </form>
+        </Card>
+      )}
+    </>
   )
 }
