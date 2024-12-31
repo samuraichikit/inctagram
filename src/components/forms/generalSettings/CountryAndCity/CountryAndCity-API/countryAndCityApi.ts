877ad92f8fd9d16dba2b1637227021a0206dde3c
Alex41099 <nevazhnoalikhan@gmail.com>
2024-12-31 20:38:24 +0500

    changed the API to a permanent one and shortened the code

--- src/components/forms/generalSettings/CountryAndCity/CountryAndCity-API/countryAndCityApi.ts
+++ src/components/forms/generalSettings/CountryAndCity/CountryAndCity-API/countryAndCityApi.ts
@@ -3,37 +3,37 @@ import { GetProfileResponse } from '@/services/profile'
 import axios from 'axios'
 
 export const countryAndCityApi = {
-  getCities(selectedValue: null | string, profile: GetProfileResponse | undefined) {
+  getCities(
+    selectedValue: null | string | undefined,
+    profile: { city?: string; country: string } | undefined
+  ) {
     return axios
-      .get(`https://www.universal-tutorial.com/api/states/${selectedValue || profile?.country}`, {
+      .get(`https://api.countrystatecity.in/v1/countries/${selectedValue}/states`, {
         headers: {
-          Accept: 'application/json',
-          Authorization:
-            'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7InVzZXJfZW1haWwiOiJuZXZhemhub2FsaWtoYW4xQGdtYWlsLmNvbSIsImFwaV90b2tlbiI6Ill6VEw4N2VES2x4LV9ibUE0R1JJUW9BSXUzcmtqZDh6c0JoTnQ3WDBEaXpKYUZfbnJOUDgwUE9oRWZ1TEpIbFJTVGsifSwiZXhwIjoxNzM1NjM3MTM4fQ.1YjVGNvvjnlwemowturBD1g9x4IW3xet_HEPY1gCdOc',
+          'X-CSCAPI-KEY': 'SnNMWVd2MkNId09vUlhtSmVFWkRGWERPNVV1eGlucVBRTGc3RTVUVg==',
         },
       })
       .then(res => {
-        const data: cityType[] = [{ state_name: profile?.city || 'city' }, ...res.data]
+        const data: cityType[] = [{ id: 1, iso2: '', name: profile?.city || 'city' }, ...res.data]
 
         return data
       })
   },
 
-  getCountries(profile: GetProfileResponse) {
+  getCountries(profile: { country: string }) {
     return axios
-      .get('https://www.universal-tutorial.com/api/countries/', {
+      .get('https://api.countrystatecity.in/v1/countries', {
         headers: {
-          Accept: 'application/json',
-          Authorization:
-            'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7InVzZXJfZW1haWwiOiJuZXZhemhub2FsaWtoYW4xQGdtYWlsLmNvbSIsImFwaV90b2tlbiI6Ill6VEw4N2VES2x4LV9ibUE0R1JJUW9BSXUzcmtqZDh6c0JoTnQ3WDBEaXpKYUZfbnJOUDgwUE9oRWZ1TEpIbFJTVGsifSwiZXhwIjoxNzM1NjM3MTM4fQ.1YjVGNvvjnlwemowturBD1g9x4IW3xet_HEPY1gCdOc',
+          'X-CSCAPI-KEY': 'SnNMWVd2MkNId09vUlhtSmVFWkRGWERPNVV1eGlucVBRTGc3RTVUVg==',
         },
       })
       .then(res => {
+        const initCity = res.data.find((c: countryType) => c.name === profile?.country)
         const data: countryType[] = [
           {
-            country_name: profile?.country || 'country',
-            country_phone_code: 1,
-            country_short_name: profile?.country || 'country',
+            id: 1,
+            iso2: initCity.iso2,
+            name: profile?.country || 'country',
           },
           ...res.data,
         ]
