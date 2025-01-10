3f1f8344908071d9a66501c062adedd79cbdc850
Alex41099 <nevazhnoalikhan@gmail.com>
2025-01-10 21:23:30 +0500

    fix page myPayments.tsx -> window undefined

--- src/components/forms/myPayments/MyPayments.tsx
+++ src/components/forms/myPayments/MyPayments.tsx
@@ -7,41 +7,27 @@ import { useGetMyPaymentsQuery } from '@/services/accountSubscriptions/accountSu
 
 import s from './MyPayments.module.scss'
 
-export const getServerSideProps = () => {
-  const windowInnerWidth = window.innerWidth
-
-  return {
-    props: {
-      windowInnerWidth,
-    },
-  }
-}
-
-type PropsType = {
-  windowInnerWidth: number
-}
-
-export const MyPayments = ({ windowInnerWidth }: PropsType) => {
+export const MyPayments = () => {
   const { data, isLoading } = useGetMyPaymentsQuery()
   const [currentPage, setCurrentPage] = useState(1)
   const [pageSize, setPageSize] = useState(5)
 
   const { t } = useTranslation()
 
-  const [init, setInit] = useState(false)
-  const [width, setWidth] = useState(windowInnerWidth)
-  const breakpoint = 1177
+  // const [init, setInit] = useState(false)
+  // const [width, setWidth] = useState(window.innerWidth)
+  // const breakpoint = 1177
 
-  useEffect(() => {
-    if (typeof window !== 'undefined') {
-      const handleWindowResize = () => setWidth(window.innerWidth)
-
-      setInit(true)
-      window.addEventListener('resize', handleWindowResize)
-
-      return () => window.removeEventListener('resize', handleWindowResize)
-    }
-  }, [])
+  // useEffect(() => {
+  //   if (typeof window !== 'undefined') {
+  //     const handleWindowResize = () => setWidth(window.innerWidth)
+  //
+  //     setInit(true)
+  //     window.addEventListener('resize', handleWindowResize)
+  //
+  //     return () => window.removeEventListener('resize', handleWindowResize)
+  //   }
+  // }, [])
 
   if (isLoading) {
     return <div>Loading...</div>
@@ -85,46 +71,40 @@ export const MyPayments = ({ windowInnerWidth }: PropsType) => {
   const currentData = pages[currentPage - 1] || []
 
   return (
-    <>
-      {!init ? (
-        <div>Loading...</div>
-      ) : (
-        <div className={s.container}>
-          {width < breakpoint ? (
-            <MobileMyPayments currentData={currentData} t={t} />
-          ) : (
-            <div className={s.blockPayments}>
-              <div className={s.blockTitle}>
-                <span className={s.one}>{t.myPayments.DateOfPayment}</span>
-                <span className={s.two}>{t.myPayments.EndDataOfSubscription}</span>
-                <span className={s.three}>{t.myPayments.Price}</span>
-                <span className={s.fo}>{t.myPayments.SubscriptionType}</span>
-                <span className={s.five}>{t.myPayments.PaymentType}</span>
-              </div>
-              {currentData.map((p, i) => (
-                <div className={s.blockInfoPrice} key={i}>
-                  <span>{p.dateOfPayment}</span>
-                  <span>{p.endDateOfSubscription}</span>
-                  <span className={s.Price}>
-                    <span>${p.price}</span>
-                  </span>
-                  <span className={s.SubsType}>{p.subscriptionType}</span>
-                  <span className={s.PaymentType}>{p.paymentType}</span>
-                </div>
-              ))}
-            </div>
-          )}
-          <Pagination
-            className={s.PaginationBlock}
-            currentPage={currentPage}
-            onPageChange={handlePageChange}
-            onPageSizeChange={handlePageSizeChange}
-            pageSize={pageSize}
-            totalCount={formattedData.length}
-          />
+    <div className={s.container}>
+      {/*{width < breakpoint ? (*/}
+      {/*  <MobileMyPayments currentData={currentData} t={t} />*/}
+      {/*) : (*/}
+      <div className={s.blockPayments}>
+        <div className={s.blockTitle}>
+          <span className={s.one}>{t.myPayments.DateOfPayment}</span>
+          <span className={s.two}>{t.myPayments.EndDataOfSubscription}</span>
+          <span className={s.three}>{t.myPayments.Price}</span>
+          <span className={s.fo}>{t.myPayments.SubscriptionType}</span>
+          <span className={s.five}>{t.myPayments.PaymentType}</span>
         </div>
-      )}
-    </>
+        {currentData.map((p, i) => (
+          <div className={s.blockInfoPrice} key={i}>
+            <span>{p.dateOfPayment}</span>
+            <span>{p.endDateOfSubscription}</span>
+            <span className={s.Price}>
+              <span>${p.price}</span>
+            </span>
+            <span className={s.SubsType}>{p.subscriptionType}</span>
+            <span className={s.PaymentType}>{p.paymentType}</span>
+          </div>
+        ))}
+      </div>
+      {/*)}*/}
+      <Pagination
+        className={s.PaginationBlock}
+        currentPage={currentPage}
+        onPageChange={handlePageChange}
+        onPageSizeChange={handlePageSizeChange}
+        pageSize={pageSize}
+        totalCount={formattedData.length}
+      />
+    </div>
   )
 }
 
