3b711d3e8e87f48d2a581f1c6565ec342fa7f6de
nikolasmushroom <dashagribok0905@gmail.com>
2025-01-21 23:37:57 +0300

    INC-119 feat: updated dropdownItem.tsx

--- src/components/ui/dropdown/DropdownItems/dropdownItem.tsx
+++ src/components/ui/dropdown/DropdownItems/dropdownItem.tsx
@@ -2,7 +2,7 @@ import { ComponentPropsWithoutRef } from 'react'
 
 import * as DropdownMenu from '@radix-ui/react-dropdown-menu'
 
-import styles from '../dropdown.module.scss'
+import s from '../dropdown.module.scss'
 
 export type DropdownItemProps = {} & ComponentPropsWithoutRef<typeof DropdownMenu.Item>
 export const DropdownItem = ({
@@ -13,7 +13,7 @@ export const DropdownItem = ({
   ...rest
 }: DropdownItemProps) => {
   return (
-    <DropdownMenu.Item className={styles.Item} {...rest} itemID={itemID} onSelect={onSelect}>
+    <DropdownMenu.Item className={s.Item} {...rest} itemID={itemID} onSelect={onSelect}>
       {children}
     </DropdownMenu.Item>
   )
