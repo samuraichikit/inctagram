cd7f49cf7c2678a69e2261d8bd69df6dc221dc48
nikolasmushroom <dashagribok0905@gmail.com>
2025-01-25 23:52:23 +0300

    INC-119 fix: cleared all socket logic

--- src/components/notificationDropdown/notificationsDropDown.tsx
+++ src/components/notificationDropdown/notificationsDropDown.tsx
@@ -1,39 +1,16 @@
-import { useCallback, useEffect, useState } from 'react'
+import { useState } from 'react'
 
 import { BellTrigger } from '@/components/notificationDropdown/bellTrigger/bellTrigger'
 import { NotificationItem } from '@/components/notificationDropdown/notificationItems/notificationItem'
 import { Dropdown } from '@/components/ui/dropdown'
 import { Typography } from '@/components/ui/typography'
-import { useGetNotificationsQuery } from '@/services/notifications/notificationsService'
 import { NotificationType } from '@/services/notifications/notificationsService.types'
-import { connectWebsockets } from '@/services/websockets/connectWebsockets'
 
-type Props = {}
-export const NotificationsDropDown = ({}: Props) => {
+type Props = {
+  notifications: NotificationType[]
+}
+export const NotificationsDropDown = ({ notifications }: Props) => {
   const [isOpen, setIsOpen] = useState(false)
-  const { data: serverNotifications } = useGetNotificationsQuery({})
-  const [notifications, setNotifications] = useState<NotificationType[]>([])
-
-  const handleNewNotification = useCallback(
-    (newNotification: NotificationType) => {
-      setNotifications(prev => [newNotification, ...prev])
-    },
-    [setNotifications]
-  )
-
-  useEffect(() => {
-    const ws = connectWebsockets()
-
-    ws.on('notification', handleNewNotification)
-    if (serverNotifications) {
-      setNotifications(serverNotifications.items)
-    }
-
-    return () => {
-      ws.off('notification', handleNewNotification)
-      ws.close()
-    }
-  }, [serverNotifications, handleNewNotification])
 
   return (
     <Dropdown
