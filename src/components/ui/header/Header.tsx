f8b4a8a31f11a18bec85749bfee18cfd7a1bac78
nikolasmushroom <dashagribok0905@gmail.com>
2025-01-25 23:55:04 +0300

    INC-119 feed: added using of useSocket logic to header component

--- src/components/ui/header/Header.tsx
+++ src/components/ui/header/Header.tsx
@@ -1,9 +1,15 @@
+import { useCallback, useEffect, useState } from 'react'
+
 import { useTranslation } from '@/common/hooks/useTranslation'
 import { LangSelect } from '@/components/langSelect/LangSelect'
 import { NotificationsDropDown } from '@/components/notificationDropdown/notificationsDropDown'
 import { Button } from '@/components/ui/button'
 import { Typography } from '@/components/ui/typography'
 import { useMeQuery } from '@/services/auth'
+import { useGetNotificationsQuery } from '@/services/notifications/notificationsService'
+import { NotificationType } from '@/services/notifications/notificationsService.types'
+import { useSocket } from '@/services/websockets/socket'
+import { getCookie } from 'cookies-next/client'
 import Link from 'next/link'
 
 import s from './header.module.scss'
@@ -17,6 +23,31 @@ export const Header = () => {
   }
 
   const { data } = useMeQuery()
+
+  const { data: serverNotifications } = useGetNotificationsQuery({
+    pageSize: 100,
+    sortDirection: 'desc',
+  })
+
+  const [notifications, setNotifications] = useState<NotificationType[]>(
+    serverNotifications?.items ?? []
+  )
+
+  useEffect(() => {
+    if (serverNotifications?.items) {
+      setNotifications(serverNotifications?.items)
+    }
+  }, [serverNotifications?.items])
+  const handleNewNotification = useCallback(
+    (notification: NotificationType) => {
+      setNotifications(prev => [notification, ...prev])
+    },
+    [setNotifications]
+  )
+  const accessToken = getCookie('accessToken') as string
+
+  useSocket(accessToken, handleNewNotification)
+
   const isMyProfile = !!data
 
   const { t } = useTranslation()
@@ -26,7 +57,7 @@ export const Header = () => {
       <div className={classNames.container}>
         <Typography variant={'large'}>Inctagram</Typography>
         <div className={classNames.navContainer}>
-          {data && <NotificationsDropDown />}
+          {data && <NotificationsDropDown notifications={notifications} />}
           <LangSelect />
           {!isMyProfile && (
             <div className={classNames.buttonsContainer}>
