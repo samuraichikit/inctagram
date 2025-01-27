5fe0b69e439c2d952738cdc858c5af5e818c6822
nikolasmushroom <dashagribok0905@gmail.com>
2025-01-27 22:58:52 +0300

    INC-119 feed: added logic of mark as read notification on notification list on close dropdown menu

--- src/components/notificationDropdown/notificationsDropDown.tsx
+++ src/components/notificationDropdown/notificationsDropDown.tsx
@@ -4,6 +4,7 @@ import { BellTrigger } from '@/components/notificationDropdown/bellTrigger/bellT
 import { NotificationItem } from '@/components/notificationDropdown/notificationItems/notificationItem'
 import { Dropdown } from '@/components/ui/dropdown'
 import { Typography } from '@/components/ui/typography'
+import { useMarkAsReadMutation } from '@/services/notifications/notificationsService'
 import { NotificationType } from '@/services/notifications/notificationsService.types'
 
 type Props = {
@@ -11,14 +12,25 @@ type Props = {
 }
 export const NotificationsDropDown = ({ notifications }: Props) => {
   const [isOpen, setIsOpen] = useState(false)
+  const [markAsRead] = useMarkAsReadMutation()
+  const handleDropdownClose = (open: boolean) => {
+    setIsOpen(open)
+    if (!open) {
+      const unreadIds = notifications.filter(n => !n.isRead).map(n => n.id)
+
+      if (unreadIds.length) {
+        markAsRead({ ids: unreadIds })
+      }
+    }
+  }
 
   return (
     <Dropdown
       align={'end'}
-      onOpenChange={setIsOpen}
+      onOpenChange={handleDropdownClose}
       open={isOpen}
       title={<Typography variant={'bold_text_14'}>Уведомления</Typography>}
-      trigger={<BellTrigger isOpen={isOpen} notificationsCount={notifications?.length} />}
+      trigger={<BellTrigger isOpen={isOpen} notifications={notifications} />}
     >
       {notifications?.map(notification => (
         <NotificationItem key={notification.id} notification={notification} />
