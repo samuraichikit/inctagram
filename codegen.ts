d884547b4dfd908a1adc2b9ac91de80d5034e94f
vustinchdev <vustinchdev@gmail.com>
2025-02-14 14:29:23 +0300

    INC-143 feat: update CodegenConfig

--- codegen.ts
+++ codegen.ts
@@ -4,14 +4,19 @@ const config: CodegenConfig = {
   schema: `https://inctagram.work/api/v1/graphql`,
   documents: ['src/**/*.{ts,tsx}'],
   generates: {
-    './src/services/admin/__generated__/': {
-      preset: 'client',
-      plugins: [],
-      presetConfig: {
-        gqlTagName: 'gql',
-      }
-    }
-  },
+    'src/services/admin/types.ts': { plugins: ['typescript'] },
+    'src/': {
+        preset: 'near-operation-file',
+        presetConfig: {
+            extension: '.generated.tsx',
+            baseTypesPath: './services/admin/types.ts',
+        },
+        plugins: ['typescript-operations', 'typescript-react-apollo'],
+        config: {
+            withHooks: true
+        }
+    },
+},
   ignoreNoDocuments: true,
 };
 
