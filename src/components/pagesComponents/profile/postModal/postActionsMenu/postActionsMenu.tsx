52c5b416f0c8d57e58133311b66167e57dc59164
Ekate <25ky-ky08@mail.ru>
2025-02-16 20:00:40 +0300

    INC-122-UI - 2

--- src/components/pagesComponents/profile/postModal/postActionsMenu/postActionsMenu.tsx
+++ src/components/pagesComponents/profile/postModal/postActionsMenu/postActionsMenu.tsx
@@ -1,49 +1,49 @@
-import { useState } from 'react'
+import {useState} from 'react'
 
-import { EditIcon } from '@/assets/icons/EditIcon'
-import { TrashIcon } from '@/assets/icons/TrashIcon'
-import { useTranslation } from '@/common/hooks/useTranslation'
-import { Button } from '@/components/ui/button'
+import {EditIcon} from '@/assets/icons/EditIcon'
+import {TrashIcon} from '@/assets/icons/TrashIcon'
+import {useTranslation} from '@/common/hooks/useTranslation'
+import {Button} from '@/components/ui/button'
 
 import s from './postActionsMenu.module.scss'
 
 type Props = {
-  showDeleteModal: (isShow: boolean) => void
-  showEditModal: (isShow: boolean) => void
+    showDeleteModal: (isShow: boolean) => void
+    showEditModal: (isShow: boolean) => void
 }
 
-export const PostActionsMenu = ({ showDeleteModal, showEditModal }: Props) => {
-  const [editModalOpen, setEditModalOpen] = useState<boolean>(false)
-  const { t } = useTranslation()
-  const toggleEditModal = () => {
-    setEditModalOpen(!editModalOpen)
-  }
+export const PostActionsMenu = ({showDeleteModal, showEditModal}: Props) => {
+    const [editModalOpen, setEditModalOpen] = useState<boolean>(false)
+    const {t} = useTranslation()
+    const toggleEditModal = () => {
+        setEditModalOpen(!editModalOpen)
+    }
 
-  const handleEditPost = () => {
-    showEditModal(true)
-    toggleEditModal()
-  }
+    const handleEditPost = () => {
+        showEditModal(true)
+        toggleEditModal()
+    }
 
-  const handleDeletePost = () => {
-    showDeleteModal(true)
-    toggleEditModal()
-  }
+    const handleDeletePost = () => {
+        showDeleteModal(true)
+        toggleEditModal()
+    }
 
-  return (
-    <div>
-      <Button className={s.toggle} onClick={toggleEditModal} variant={'icon'}>
-        {'...'}
-      </Button>
-      {editModalOpen && (
-        <div className={s.editModal}>
-          <Button className={s.btn} onClick={handleEditPost} variant={'icon'}>
-            <EditIcon /> {t.postModal.editPost}
-          </Button>
-          <Button className={s.btn} onClick={handleDeletePost} variant={'icon'}>
-            <TrashIcon /> {t.postModal.deletePost}
-          </Button>
+    return (
+        <div>
+            <Button className={s.toggle} onClick={toggleEditModal} variant={'icon'}>
+                {'...'}
+            </Button>
+            {editModalOpen && (
+                <div className={s.editModal}>
+                    <Button className={s.btn} onClick={handleEditPost} variant={'icon'}>
+                        <EditIcon/> {t.postModal.editPost}
+                    </Button>
+                    <Button className={s.btn} onClick={handleDeletePost} variant={'icon'}>
+                        <TrashIcon/> {t.postModal.deletePost}
+                    </Button>
+                </div>
+            )}
         </div>
-      )}
-    </div>
-  )
+    )
 }
