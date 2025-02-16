96e4938e4ae4f5cdf29c4d92465b2d6f189fc467
vustinchdev <vustinchdev@gmail.com>
2025-02-16 16:17:34 +0300

    INC-126 feat: add styles for AdminUserInfo

--- src/components/pagesComponents/admin/adminUserInfo/adminUserInfo.tsx
+++ src/components/pagesComponents/admin/adminUserInfo/adminUserInfo.tsx
@@ -3,8 +3,19 @@ import { Avatar } from '@/components/ui/avatar'
 import { Typography } from '@/components/ui/typography'
 import { useGetUserQuery } from '@/services/admin/usersService.generated'
 import { useRouter } from 'next/router'
+import s from './adminUserInfo.module.scss'
 
 export const AdminUserInfo = () => {
+  const classNames = {
+    userDetailsContainer: s.userDetailsContainer,
+    accountInfoContainer: s.accountInfoContainer,
+    userIdContainer: s.userIdContainer,
+    creationDateContainer: s.creationDateContainer,
+    userName: s.userName,
+    adminUserInfoContainer: s.adminUserInfoContainer,
+    userId: s.userId,
+    creationDate: s.creationDate,
+  }
   const { query } = useRouter()
   const userId = Number(query.id)
   const { data } = useGetUserQuery({ variables: { userId: userId } })
@@ -16,18 +27,30 @@ export const AdminUserInfo = () => {
   const { avatars, createdAt, firstName, id, lastName, userName } = data.getUser.profile
 
   return (
-    <div>
-      <Avatar height={60} src={avatars?.[0]?.url} width={60} />
-      <Typography asChild variant={'h1'}>
-        <h1>
-          {firstName} {lastName}
-        </h1>
-      </Typography>
-      <Typography variant={'regular_link'}>{userName}</Typography>
-      <Typography>UserID</Typography>
-      <Typography variant={'regular_text_16'}>{id}</Typography>
-      <Typography>Profile Creation Date</Typography>
-      <Typography variant={'regular_text_16'}>{formatDate(createdAt)}</Typography>
+    <div className={classNames.adminUserInfoContainer}>
+      <div className={classNames.userDetailsContainer}>
+        <Avatar height={60} src={avatars?.[0]?.url} width={60} />
+        <div>
+          <Typography asChild variant={'h1'}>
+            <h1>
+              {firstName} {lastName}
+            </h1>
+          </Typography>
+          <Typography variant={'regular_link'} className={classNames.userName}>
+            {userName}
+          </Typography>
+        </div>
+      </div>
+      <div className={classNames.accountInfoContainer}>
+        <div className={classNames.userIdContainer}>
+          <Typography className={classNames.userId}>UserID</Typography>
+          <Typography variant={'regular_text_16'}>{id}</Typography>
+        </div>
+        <div className={classNames.creationDateContainer}>
+          <Typography className={classNames.creationDate}>Profile Creation Date</Typography>
+          <Typography variant={'regular_text_16'}>{formatDate(createdAt)}</Typography>
+        </div>
+      </div>
     </div>
   )
 }
