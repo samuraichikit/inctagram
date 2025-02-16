c709455074430368c8b8bd6c6e971638bd3a0280
vustinchdev <vustinchdev@gmail.com>
2025-02-16 10:04:07 +0300

    INC-144 feat: add authorization header to Apollo Client

--- src/services/admin/client.ts
+++ src/services/admin/client.ts
@@ -1,7 +1,10 @@
-import { BASE_URL } from '@/common/constants'
+import { ADMIN_ENCODED_CREDENTIALS, BASE_URL } from '@/common/constants'
 import { ApolloClient, InMemoryCache } from '@apollo/client'
 
 export const client = new ApolloClient({
   cache: new InMemoryCache(),
   uri: `${BASE_URL}v1/graphql`,
+  headers: {
+    Authorization: `Basic ${ADMIN_ENCODED_CREDENTIALS}`
+  }
 })
