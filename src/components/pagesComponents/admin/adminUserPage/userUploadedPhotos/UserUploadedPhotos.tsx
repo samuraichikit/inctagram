91b1e87fc8b0bfdf88de471a6b1153332a8eea31
vustinchdev <vustinchdev@gmail.com>
2025-02-17 21:56:39 +0300

    INC-126 feat: add styles for userUploadPhotos

--- src/components/pagesComponents/admin/adminUserPage/userUploadedPhotos/UserUploadedPhotos.tsx
+++ src/components/pagesComponents/admin/adminUserPage/userUploadedPhotos/UserUploadedPhotos.tsx
@@ -3,10 +3,15 @@ import { useGetPostsByUserQuery } from '@/services/admin/postsService.generated'
 import Image from 'next/image'
 import { useRouter } from 'next/router'
 
+import s from './userUploadedPhotos.module.scss'
+
 export const UserUploadedPhotos = () => {
+  const classNames = {
+    container: s.container,
+  }
   const { query } = useRouter()
   const userId = Number(query.id)
-  const { data } = useGetPostsByUserQuery({ variables: { userId } })
+  const { data } = useGetPostsByUserQuery({ variables: { userId: 1758 } })
 
   if (!data?.getPostsByUser) {
     return null
@@ -14,15 +19,19 @@ export const UserUploadedPhotos = () => {
 
   const { items } = data.getPostsByUser
 
-  return items?.map(item => {
-    return (
-      <Image
-        alt={`Image uploaded on ${formatDate(item.createdAt)}`}
-        height={228}
-        key={item.id}
-        src={item.url ?? ''}
-        width={234}
-      />
-    )
-  })
+  return (
+    <div className={classNames.container}>
+      {items?.map(item => {
+        return (
+          <Image
+            alt={`Image uploaded on ${formatDate(item.createdAt)}`}
+            height={228}
+            key={item.id}
+            src={item.url ?? ''}
+            width={234}
+          />
+        )
+      })}
+    </div>
+  )
 }
