8a236b9b9fdd07289dafae4bb39760b46bd621e0
vustinchdev <vustinchdev@gmail.com>
2025-02-18 11:39:36 +0300

    INC-126 feat: replace div with Link in BackToUsersListLink

--- src/components/pagesComponents/admin/adminUserPage/backToUserListLink/BackToUsersListLink.tsx
+++ src/components/pagesComponents/admin/adminUserPage/backToUserListLink/BackToUsersListLink.tsx
@@ -1,15 +1,16 @@
 import { ArrowLeftIcon } from '@/assets/icons/ArrowLeftIcon'
 import { Typography } from '@/components/ui/typography'
 import s from './backToUsersListLink.module.scss'
+import Link from 'next/link'
 
 export const BackToUsersListLink = () => {
   const classNames = {
     container: s.container,
   }
   return (
-    <div className={classNames.container}>
+    <Link href={'#'} className={classNames.container}>
       <ArrowLeftIcon />
       <Typography variant="medium_text_14">Back to Users List</Typography>
-    </div>
+    </Link>
   )
 }
