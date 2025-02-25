a453fb5d449acfe5674005ec146d85102885e250
nikolasmushroom <dashagribok0905@gmail.com>
2025-02-25 13:10:26 +0300

    INC-119 refactor: finale refactor websockets

--- src/common/hooks/useSocket.ts
+++ src/common/hooks/useSocket.ts
@@ -1,48 +1,26 @@
-import { useCallback, useEffect, useRef, useState } from 'react'
+import { useEffect, useState } from 'react'
 
-import { WS_EVENT_PATH } from '@/common/constants/websocketConstants'
-import { createSocket } from '@/services/socketService/createSocket'
-import { Socket } from 'socket.io-client'
+import { WEBSOCKET_URL } from '@/common/constants'
+import { Socket, io } from 'socket.io-client'
 
 export const useSocket = (accessToken: string) => {
   const [socket, setSocket] = useState<Socket | null>(null)
-  const socketRef = useRef<Socket | null>(null)
-  const [connected, setConnected] = useState(false)
-  const connectSocket = useCallback(() => {
-    if (accessToken && !socketRef.current) {
-      const newSocket = createSocket(accessToken)
-
-      newSocket.on(WS_EVENT_PATH.CONNECT, () => setConnected(true))
-      newSocket.on(WS_EVENT_PATH.DISCONNECT, () => setConnected(false))
-
-      socketRef.current = newSocket
-      setSocket(newSocket)
-    }
-  }, [accessToken])
 
   useEffect(() => {
-    if (accessToken) {
-      connectSocket()
+    const queryParams = {
+      query: {
+        accessToken: accessToken,
+      },
     }
+
+    const newSocket = io(WEBSOCKET_URL, queryParams)
+
+    setSocket(newSocket)
 
     return () => {
-      if (socketRef.current) {
-        socketRef.current.disconnect()
-      }
+      newSocket.disconnect()
     }
-  }, [accessToken, connectSocket])
-
-  const reconnectSocket = useCallback(() => {
-    if (!connected && accessToken) {
-      socketRef.current?.connect()
-    }
-  }, [connected, accessToken])
-
-  useEffect(() => {
-    if (!connected && accessToken) {
-      reconnectSocket()
-    }
-  }, [connected, accessToken, reconnectSocket])
+  }, [accessToken])
 
   return { socket }
 }
