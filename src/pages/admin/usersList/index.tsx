d8c53f4e41023ce091a6998b487567fe35bb94fc
vustinchdev <vustinchdev@gmail.com>
2025-03-11 20:30:29 +0300

    INC-126 fix: resolve line ending issues for code style consistency

--- src/pages/admin/usersList/index.tsx
+++ src/pages/admin/usersList/index.tsx
@@ -2,7 +2,6 @@ import { UserList } from '@/components/pagesComponents/admin/usersList'
 import { getAdminLayout } from '@/components/ui/layout/adminLayout'
 import { NextPageWithLayout } from '@/pages/_app'
 
-
 const UsersList: NextPageWithLayout = () => (
   <div>
     <UserList />
