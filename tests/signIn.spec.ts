b9782bd6f9557fdeed4174b9c921ebf321374f8c
e-nie <eniehaus111@gmail.com>
2025-03-26 02:37:33 +0100

    fix test

--- tests/signIn.spec.ts
+++ tests/signIn.spec.ts
@@ -2,10 +2,14 @@ import { expect, test } from '@playwright/test'
 
 test('should sign in', async ({ page }) => {
     await page.goto('http://localhost:3000/auth/signIn')
-    await page.getByTestId('email').fill('sejad87048@evluence.com')
-    await page.getByTestId('password').fill('Test!1234')
+    // await page.goto('http://www.samuraichiki.ors/en/auth/signIn')
+    // await page.getByTestId('email').fill('sejad87048@evluence.com')
+    await page.getByTestId('email').fill('vl1vl@yahoo.com')
+    await page.getByTestId('password').fill('Reverie!222')
     await page.getByTestId('signIn').click()
-    expect(await page.title()).toBe('Home')
+    expect(await page.locator('text=Evchen').isVisible())
 
 })
+
+
 
