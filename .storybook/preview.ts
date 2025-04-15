b4f139835cfbbf1a8a7d8572172ee56be2263e46
vustinchdev <vustinchdev@gmail.com>
2025-04-15 16:48:09 +0300

    INC-199 feat: update import styles in preview.ts

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,6 +1,6 @@
 import type { Preview } from '@storybook/react'
 import { themes } from '@storybook/theming'
-import '../src/styles/index.scss'
+import '@samuraichikit/inc-ui-kit/dist/index.css'
 
 const preview: Preview = {
   parameters: {
