f8026c3e42b81652986d552731348a9a5fa72bd3
Ekate <25ky-ky08@mail.ru>
2025-04-20 17:37:09 +0300

    delete userList sideBar datepicker icons lang

--- src/components/ui/sidebar/mainSidebar/MainSidebar.tsx
+++ src/components/ui/sidebar/mainSidebar/MainSidebar.tsx
@@ -9,13 +9,12 @@ import { TrendingUpOutlineIcon } from '@/assets/icons/TrendingUpOutline'
 import { useTranslation } from '@/common/hooks/useTranslation'
 import { CreatePost } from '@/components/pagesComponents/createPost/CreatePost'
 import { useMeQuery } from '@/services/auth'
-import { SearchIcon } from '@samuraichikit/inc-ui-kit'
+import { SearchIcon, Sidebar } from '@samuraichikit/inc-ui-kit'
+import { SidebarItem } from '@samuraichikit/inc-ui-kit/dist/components/sidebar/sidebarItem'
 
 import s from './mainSidebar.module.scss'
 
-import { Sidebar } from '..'
 import { LogoutButton } from '../logoutButton'
-import { SidebarItem } from '../sidebarItem'
 
 export const MainSidebar = () => {
   const classNames = {
