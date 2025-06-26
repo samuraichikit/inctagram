3f6d5c11d719c0aec90a0b91bd725267da2c4069
vustinchdev <vustinchdev@gmail.com>
2025-06-26 12:08:28 +0300

    INC-224 feat: add UserSearchItem component

---
+++ src/components/pagesComponents/userSearch/userSearchItem/UserSearchItem.tsx
@@ -0,0 +1,38 @@
+import { Avatar } from '@/components/ui/avatar'
+import { DefaultAvatar, Typography } from '@samuraichikit/inc-ui-kit'
+
+import s from './userSearchItem.module.scss'
+
+type Props = {
+  firsName: string
+  lastName: string
+  src?: string
+  userName: string
+}
+
+export const UserSearchItem = ({ firsName, lastName, src, userName }: Props) => {
+  const classNames = {
+    container: s.container,
+    defaultAvatar: s.defaultAvatar,
+    userFullname: s.userFullname,
+    userName: s.userName,
+  }
+
+  return (
+    <div className={classNames.container}>
+      {src ? (
+        <Avatar height={48} src={src} width={48} />
+      ) : (
+        <DefaultAvatar className={classNames.defaultAvatar} />
+      )}
+      <div>
+        <Typography className={classNames.userName} variant={'bold_text_14'}>
+          {userName}
+        </Typography>
+        <Typography className={classNames.userFullname}>
+          {firsName} {lastName}
+        </Typography>
+      </div>
+    </div>
+  )
+}
