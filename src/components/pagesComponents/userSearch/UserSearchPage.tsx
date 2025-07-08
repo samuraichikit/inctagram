daf835d5cb1478c888d9df3b04bab88d09341fec
vustinchdev <vustinchdev@gmail.com>
2025-07-08 09:39:26 +0300

    INC-224 feat: add user search functionality

---
+++ src/components/pagesComponents/userSearch/UserSearchPage.tsx
@@ -0,0 +1,50 @@
+import { ChangeEvent, useState } from 'react'
+
+import { useTranslation } from '@/common/hooks/useTranslation'
+import { useGetUsersProfilesQuery } from '@/services/usersFollowingAndFollowers'
+import { TextField, Typography } from '@samuraichikit/inc-ui-kit'
+
+import s from './userSearch.module.scss'
+
+import { UserSearchItem } from './userSearchItem'
+
+export const UserSearchPage = () => {
+  const classNames = {
+    textField: s.textField,
+    usersProfilesContainer: s.usersProfilesContainer,
+  }
+  const [search, setSearch] = useState('')
+  const { t } = useTranslation()
+  const { data } = useGetUsersProfilesQuery({ pageSize: 14, search }, { skip: !search })
+  const usersProfiles = data?.items
+
+  const searchHandler = (e: ChangeEvent<HTMLInputElement>) => {
+    setSearch(e.currentTarget.value)
+  }
+
+  return (
+    <>
+      <Typography asChild variant={'h1'}>
+        <h1>{t.searchPage.search}</h1>
+      </Typography>
+      <TextField
+        className={classNames.textField}
+        onChange={searchHandler}
+        placeholder={t.searchPage.search}
+        type={'search'}
+        value={search}
+      />
+      <div className={classNames.usersProfilesContainer}>
+        {usersProfiles?.map(({ avatars, firstName, id, lastName, userName }) => (
+          <UserSearchItem
+            firsName={firstName}
+            key={id}
+            lastName={lastName}
+            src={avatars[0]?.url}
+            userName={userName}
+          />
+        ))}
+      </div>
+    </>
+  )
+}
