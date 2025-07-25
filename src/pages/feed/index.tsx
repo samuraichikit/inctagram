0315985b8426201f16dd8c52e53a8116d6b56385
vustinchdev <vustinchdev@gmail.com>
2025-07-25 08:43:39 +0300

    INC-225 feat: add page feed

---
+++ src/pages/feed/index.tsx
@@ -0,0 +1,7 @@
+import { getBaseLayout } from '@/components/ui/layout'
+import { NextPageWithLayout } from '@/pages/_app'
+
+const Feed: NextPageWithLayout = () => <>Feed</>
+
+Feed.getLayout = getBaseLayout
+export default Feed
