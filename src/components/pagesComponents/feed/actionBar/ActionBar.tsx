a18c2e8a0aea03a5884e8519c9ca89d4cc8b6a9f
vustinchdev <vustinchdev@gmail.com>
2025-07-27 16:09:03 +0300

    INC-225 feat: add styles to action bar component

--- src/components/pagesComponents/feed/actionBar/ActionBar.tsx
+++ src/components/pagesComponents/feed/actionBar/ActionBar.tsx
@@ -2,21 +2,30 @@ import { HeartIcon } from '@/assets/icons/HeartIcon'
 import { PaperPlaneIcon } from '@radix-ui/react-icons'
 import { BookmarkOutlineIcon, Button, MessengerIcon } from '@samuraichikit/inc-ui-kit'
 
+import s from './actionBar.module.scss'
+
 export const ActionBar = () => {
+  const classNames = {
+    container: s.container,
+    iconsWrapper: s.iconsWrapper,
+  }
+
   return (
-    <>
-      <Button variant={'icon'}>
-        <HeartIcon />
-      </Button>
-      <Button variant={'icon'}>
-        <MessengerIcon />
-      </Button>
-      <Button variant={'icon'}>
-        <PaperPlaneIcon />
-      </Button>
+    <div className={classNames.container}>
+      <div className={classNames.iconsWrapper}>
+        <Button variant={'icon'}>
+          <HeartIcon />
+        </Button>
+        <Button variant={'icon'}>
+          <MessengerIcon />
+        </Button>
+        <Button variant={'icon'}>
+          <PaperPlaneIcon />
+        </Button>
+      </div>
       <Button variant={'icon'}>
         <BookmarkOutlineIcon />
       </Button>
-    </>
+    </div>
   )
 }
