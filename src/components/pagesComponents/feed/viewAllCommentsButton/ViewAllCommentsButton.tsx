f15bd150762dc7ba27d314b34dbe739b6e19661f
vustinchdev <vustinchdev@gmail.com>
2025-07-27 20:52:05 +0300

    INC-225 feat: add styles to view all comments button component

--- src/components/pagesComponents/feed/viewAllCommentsButton/ViewAllCommentsButton.tsx
+++ src/components/pagesComponents/feed/viewAllCommentsButton/ViewAllCommentsButton.tsx
@@ -1,5 +1,15 @@
 import { Typography } from '@samuraichikit/inc-ui-kit'
 
+import s from './viewAllComments.module.scss'
+
 export const ViewAllCommentsButton = () => {
-  return <Typography>View all comments</Typography>
+  const classNames = {
+    text: s.text,
+  }
+
+  return (
+    <Typography className={classNames.text} variant={'bold_text_14'}>
+      View all comments
+    </Typography>
+  )
 }
