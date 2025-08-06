083bf31c10a6d5c07db4d6018ea4e51166710c05
Ekate <25ky-ky08@mail.ru>
2025-08-06 03:19:06 +0300

    fix: comments - 2

--- src/services/posts/postsService.ts
+++ src/services/posts/postsService.ts
@@ -77,6 +77,6 @@ export const {
   useGetPostMessageByIdQuery,
   useGetUserPostsQuery,
   useLazyGetUserPostsQuery,
-  useUpdatePostMutation,
   useUpdateLikeStatusMutation,
+  useUpdatePostMutation,
 } = postService
