e44c22fdb5991b90db8defa2e86dfaef790e8fd2
Ekate <25ky-ky08@mail.ru>
2025-08-18 17:41:03 +0300

    fix-public modal

--- src/components/pagesComponents/publicProfile/publicPostModal/PublicPostModal.tsx
+++ src/components/pagesComponents/publicProfile/publicPostModal/PublicPostModal.tsx
@@ -43,7 +43,7 @@ export const PublicPostModal = ({ isOpen, onClose, postId }: Props) => {
           <div className={classNames.userInfoContainer}>
             <UserInfo src={avatarOwner} userName={userName} />
           </div>
-          <Comments comments={comments} />
+          <Comments avatarOwner={''} comments={comments} userName={''} />
           <PostLikes
             avatarsSrc={avatarWhoLikes}
             className={classNames.postLikes}
