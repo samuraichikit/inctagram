dbb13481043424bdf80d7e0a9b828d0e3e2f0627
vustinchdev <vustinchdev@gmail.com>
2025-09-22 21:42:35 +0300

    INC-234 feat: implement add message after sending

--- src/common/hooks/useConnectSocket.ts
+++ src/common/hooks/useConnectSocket.ts
@@ -1,10 +1,13 @@
 import { useEffect } from 'react'
 
+import { useAppDispatch } from '@/app/store'
 import { socketApi } from '@/services/socket/socketApi'
 import { registerSocketListeners } from '@/services/socket/socketListeners'
 import { getCookie } from 'cookies-next/client'
 
 export const useConnectSocket = () => {
+  const dispatch = useAppDispatch()
+
   useEffect(() => {
     const accessToken = getCookie('accessToken') as string
 
@@ -15,7 +18,7 @@ export const useConnectSocket = () => {
     socketApi.connect(accessToken)
 
     socketApi.on('connect', () => {
-      registerSocketListeners()
+      registerSocketListeners(dispatch)
     })
 
     return () => {
--- src/services/socket/socketListeners.ts
+++ src/services/socket/socketListeners.ts
@@ -1,13 +1,16 @@
+import { AppDispatch } from '@/app/store'
 import { WS_EVENT_PATH } from '@/common/constants/wsEventPath'
 import { Message } from '@/services/messenger/messengerService.types'
 import { socketApi } from '@/services/socket/socketApi'
+
+import { messengerService } from '../messenger'
 
 type Data = {
   message: Message
   receiverId: number
 }
 
-export const registerSocketListeners = () => {
+export const registerSocketListeners = (dispatch: AppDispatch) => {
   const cleanup = () => {
     socketApi.off(WS_EVENT_PATH.MESSAGE_SEND)
     socketApi.off(WS_EVENT_PATH.RECEIVE_MESSAGE)
@@ -16,11 +19,41 @@ export const registerSocketListeners = () => {
   cleanup()
 
   socketApi.on(WS_EVENT_PATH.MESSAGE_SEND, (message: Message, callback: (data: Data) => void) => {
+    dispatch(
+      messengerService.util.updateQueryData(
+        'getMessagesById',
+        { dialoguePartnerId: message.ownerId },
+        draft => {
+          const index = draft.items.findIndex(m => m.id === message.id)
+
+          if (index === -1) {
+            draft.items.unshift(message)
+          } else {
+            draft.items[index] = message
+          }
+        }
+      )
+    )
     callback({ message, receiverId: message.receiverId })
   })
 
-  socketApi.on(WS_EVENT_PATH.RECEIVE_MESSAGE, (updatedMessage: Message) => {
-    console.log('RECEIVE_MESSAGE:', updatedMessage)
+  socketApi.on(WS_EVENT_PATH.RECEIVE_MESSAGE, (message: Message) => {
+    dispatch(
+      messengerService.util.updateQueryData(
+        'getMessagesById',
+        {
+          dialoguePartnerId:
+            message.ownerId === message.receiverId ? message.ownerId : message.receiverId,
+        },
+        draft => {
+          const exists = draft.items.some(m => m.id === message.id)
+
+          if (!exists) {
+            draft.items.unshift(message)
+          }
+        }
+      )
+    )
   })
 
   return cleanup
